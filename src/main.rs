ee6671ba8a7af440204183f9b921dfd2ce9d1618
abc <danielmurphy50@gmail.com>
2024-06-06 23:20:27 +0100

    Added basic error handleing Added sample openapi file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use axum::{
-    extract::Request,
+    extract::{rejection::JsonRejection, FromRequest, MatchedPath, Request, State},
     response::{IntoResponse, Response},
     routing::get,
     Router,
@@ -8,30 +8,63 @@ use bytes::Bytes;
 use http_body_util::Empty;
 use hyper::StatusCode;
 use hyper_util::rt::TokioIo;
-use std::collections::HashMap;
-use tokio::net::TcpStream;
+use oas3::Spec;
+use serde::{Deserialize, Serialize};
+use std::env;
+use std::io::Error as IOError;
+use tokio::{net::TcpStream, stream};
+use tower_http::trace::TraceLayer;
 
 #[tokio::main]
 async fn main() {
-    let mut routes: HashMap<String, OpenAPIObject> = HashMap::new();
+    // this method needs to be inside main() method
+    env::set_var("RUST_BACKTRACE", "1");
+
+    let spec =
+        oas3::from_path("./resources/OpenAPISpecExamples/pet_store.json".to_string()).unwrap();
 
     //Need to read the openapi spec and save the results into useable objects
 
-    let app = Router::new().route("/*segements", get(handler_segments));
+    let app = Router::new()
+        .route("/", get(handler_segments))
+        .layer(
+            TraceLayer::new_for_http()
+                // Create our own span for the request and include the matched path. The matched
+                // path is useful for figuring out which handler the request was routed to.
+                .make_span_with(|req: &Request| {
+                    let method = req.method();
+                    let uri = req.uri();
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+                    // axum automatically adds this extension.
+                    let matched_path = req
+                        .extensions()
+                        .get::<MatchedPath>()
+                        .map(|matched_path| matched_path.as_str());
+
+                    tracing::debug_span!("request", %method, %uri, matched_path)
+                })
+                // By default `TraceLayer` will log 5xx responses but we're doing our specific
+                // logging of errors so disable that
+                .on_failure(()),
+        )
+        .with_state(spec);
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
         .await
         .unwrap();
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
 
-struct OpenAPIObject {
-    to: String,
-}
+async fn handler_segments(State(spec): State<Spec>, req: Request) -> Result<Response, AppError> {
+    let path = req.uri().path();
 
-async fn handler_segments(mut req: Request) -> Result<Response, StatusCode> {
-    let url = "https://8.8.8.8".parse::<hyper::Uri>().unwrap();
+    match spec.paths.contains_key(path) {
+        true => println!("true-{}", &path),
+        false => println!("false-{}", &path),
+    }
+
+    let url = "http://localhost:3000".parse::<hyper::Uri>().unwrap();
 
     let host = url.host().expect("uri has no host");
     let port = url.port_u16().unwrap_or(80);
@@ -40,11 +73,14 @@ async fn handler_segments(mut req: Request) -> Result<Response, StatusCode> {
 
     println!("Address: {}", address);
 
-    let stream = TcpStream::connect(address).await;
+    //let stream = TcpStream::connect(address).await;
 
     // Use an adapter to access something implementing `tokio::io` traits as if they implement
     // `hyper::rt` IO traits.
-    let io = TokioIo::new(stream.unwrap());
+
+    let mut stream = TcpStream::connect(address).await?;
+
+    let io = TokioIo::new(stream);
 
     let handshake_result = hyper::client::conn::http1::handshake(io).await;
 
@@ -67,9 +103,92 @@ async fn handler_segments(mut req: Request) -> Result<Response, StatusCode> {
     println!("Req status: {:?}", req);
 
     // Await the response...
-    let mut res = sender.send_request(req).await.unwrap();
+    let mut res = match sender.send_request(req).await {
+        Ok(value) => println!("Result:"),
+        Err(error) => println!("Error: {}", error),
+    };
 
     println!("Response status: {:?}", res);
 
     Ok(res.into_response())
+}
+
+// The kinds of errors we can hit in our application.
+enum AppError {
+    // The request body contained invalid JSON
+    JsonRejection(JsonRejection),
+    HyperRejection(hyper::Error),
+    IORejection(IOError),
+}
+
+// Tell axum how `AppError` should be converted into a response.
+//
+// This is also a convenient place to log errors.
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        // How we want errors responses to be serialized
+        #[derive(Serialize)]
+        struct ErrorResponse {
+            message: String,
+        }
+
+        let (status, message) = match self {
+            AppError::JsonRejection(rejection) => {
+                // This error is caused by bad user input so don't log it
+                (rejection.status(), rejection.body_text())
+            }
+            AppError::HyperRejection(err) => {
+                tracing::error!(%err, "error from hyper");
+
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    "Something went wrong".to_owned(),
+                )
+            }
+            AppError::IORejection(err) => {
+                tracing::error!(%err, "error from hyper");
+
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    "Something went wrong".to_owned(),
+                )
+            }
+        };
+
+        (status, AppJson(ErrorResponse { message })).into_response()
+    }
+}
+
+impl From<JsonRejection> for AppError {
+    fn from(rejection: JsonRejection) -> Self {
+        Self::JsonRejection(rejection)
+    }
+}
+
+impl From<hyper::Error> for AppError {
+    fn from(error: hyper::Error) -> Self {
+        Self::HyperRejection(error)
+    }
+}
+
+impl From<IOError> for AppError {
+    fn from(error: IOError) -> Self {
+        Self::IORejection(error)
+    }
+}
+
+// Create our own JSON extractor by wrapping `axum::Json`. This makes it easy to override the
+// rejection and provide our own which formats errors to match our application.
+//
+// `axum::Json` responds with plain text if the input is invalid
+
+struct AppJson<T>(T);
+
+impl<T> IntoResponse for AppJson<T>
+where
+    axum::Json<T>: IntoResponse,
+{
+    fn into_response(self) -> Response {
+        axum::Json(self.0).into_response()
+    }
 }
